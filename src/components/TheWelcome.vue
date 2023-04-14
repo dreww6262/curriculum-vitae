d7c6ed148228cf7133ce71a150130e063d411d3c
Andrew Williamson <ae.williamson16@gmail.com>
2023-04-14 17:06:25 -0400

    dist

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,11 +4,14 @@ import Icon from './Icon.vue'
 </script>
 
 <template>
+  <div class="welcomeContainer">
   <WelcomeItem>
     <template #icon>
       <Icon imgName="reprise.avif" />
     </template>
-    <template #heading> <a href="http://www.reprise.com" target="_blank" rel="noopener">Reprise</a></template>
+    <template #heading>
+      <a href="http://www.reprise.com" target="_blank" rel="noopener">Reprise</a></template
+    >
     <p>Feb 2022 - Jan 2023 | Full Stack Engineer</p>
     <ul>
       <li>Implemented full stack features on the company's flagship Demo Editor</li>
@@ -21,7 +24,11 @@ import Icon from './Icon.vue'
     <template #icon>
       <Icon imgName="apokoz.jpg" />
     </template>
-    <template #heading><a href="https://github.com/akinpoz/akinpoz-mvp">Apokoz</a></template>
+    <template #heading
+      ><a href="https://github.com/akinpoz/akinpoz-mvp" target="_blank" rel="noopener"
+        >Apokoz</a
+      ></template
+    >
 
     <p>May 2021 - Dec 2021 | Founder & Chief Product Officer</p>
     <ul>
@@ -35,11 +42,15 @@ import Icon from './Icon.vue'
     <template #icon>
       <Icon imgName="bio.jpg" />
     </template>
-    <template #heading><a href="https://github.com/dreww6262/Bio">Bio</a></template>
+    <template #heading
+      ><a href="https://github.com/dreww6262/Bio" target="_blank" rel="noopener">Bio</a></template
+    >
 
     <p>May 2020 - May 2021 | Founder & Chief Technical Officer</p>
     <ul>
-      <li>Created a product geared towards self starters and influencers to promote their brand.</li>
+      <li>
+        Created a product geared towards self starters and influencers to promote their brand.
+      </li>
       <li>Implemented a full stack MVP using Swift, Kotlin, and Google Cloud Platform.</li>
     </ul>
   </WelcomeItem>
@@ -48,12 +59,34 @@ import Icon from './Icon.vue'
     <template #icon>
       <Icon imgName="booz_allen.jpg" />
     </template>
-    <template #heading><a href="https://www.boozallen.com/expertise/products/cybersecurity-products/district-defend.html">Booz Allen | District Defend</a></template>
+    <template #heading
+      ><a
+        href="https://www.boozallen.com/expertise/products/cybersecurity-products/district-defend.html"
+        target="_blank"
+        rel="noopener"
+        >Booz Allen | District Defend</a
+      ></template
+    >
 
     <p>May 2019 - Aug 2019 | Software Development Intern</p>
     <ul>
-      <li>Worked in an Agile workflow to create production components using AngularDart on top of a .NET Core framework.</li>
+      <li>
+        Worked in an Agile workflow to create production components using AngularDart on top of a
+        .NET Core framework.
+      </li>
     </ul>
   </WelcomeItem>
-
+  </div>
 </template>
+
+<style scoped lang="scss">
+.welcomeContainer {
+  margin: 0 auto;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  overflow-y: auto;
+  padding-left: 30px;
+}
+</style>
--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -14,7 +14,7 @@
 
 <style scoped>
 .item {
-  margin-top: 2rem;
+  padding-top: 2rem;
   display: flex;
 }
 
@@ -44,6 +44,7 @@ h3 {
   .item {
     margin-top: 0;
     padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    display: block;
   }
 
   i {
