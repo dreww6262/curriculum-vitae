cbb62bf5e5be195d9a4b81d61de5e409e24d5638
Andrew Williamson <ae.williamson16@gmail.com>
2023-04-17 17:00:37 -0400

    setup prod env

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const base_url = process.env.VITE_mode === 'prod' ? '/curriculum-vitae/' : "/"
+  return defineConfig({
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL(`./src`, import.meta.url))
+      }
+    },
+    base: base_url
+  })
+}
